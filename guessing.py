872b294826e8b63810668cec4df2a2e48d0bba71
ARUNAW_RISHE_2024_aia <arunawrishe@gmail.com>
2024-11-03 10:44:25 +0530

    change

--- guessing.py
+++ guessing.py
@@ -94,7 +94,7 @@ def machine_guessing_game():
 if st.session_state['test'] == 0:
     
     st.markdown("<h1 style='text-align:center; color:indigo; font-weight:bold;'>PORTFOLIO</h1>", unsafe_allow_html=True)
-    image="C:\Users\aruna\Downloads\IMG_20241031_113601_647.webp"
+    image=r"C:\Users\aruna\Downloads\IMG_20241031_113601_647.webp"
     col1, col2 = st.columns([3, 1])
         
     with col2:
